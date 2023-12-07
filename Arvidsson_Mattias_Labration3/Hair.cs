5dc1af6e2cd298127f6c2a249386a742a35494d0
h23maarv <h23maarv@du.se>
2023-12-07 09:22:24 +0100

    Flyttade till eget repository

---
+++ Arvidsson_Mattias_Labration3/Hair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvidsson_Mattias_Labration3
+{
+    // En strukt som representerar hårdetaljer med attributen HairLength (hårlängd i centimeter) och HairColor (hårfärg).
+    public struct Hair
+    {
+        // Attribut för hårlängd i centimeter.
+        public int HairLength;
+        // Attribut för hårfärg.
+        public string HairColor;
+    }
+}
